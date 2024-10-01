33dee04b9d0787727908fc142a43093831da15d3
serPeterDuenas <serpeterduenas@gmail.com>
2024-09-30 19:39:15 -0700

    added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection.normalized * moveSpeed; 
     }
 }
