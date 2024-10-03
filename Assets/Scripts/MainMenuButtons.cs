5063110c9159e03672f6e5c6598ef208a1a003dd
serPeterDuenas <serpeterduenas@gmail.com>
2024-10-02 18:55:30 -0700

    implemented main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
