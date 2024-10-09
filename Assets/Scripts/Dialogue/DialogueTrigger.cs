3dd555bf2d38b8e10c4319026fff5c90dbe7345a
serPeterDuenas <serpeterduenas@gmail.com>
2024-10-08 22:08:05 -0700

    refactoring dialogue system

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+    }
+}
